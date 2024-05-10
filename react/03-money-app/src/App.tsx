dd307c70d6fa8706186febac0fd703361d4d4642
Rosivaldo Lucas <rosivaldo@twinfo.io>
2024-05-10 15:15:09 -0300

    Adiciona pagina Transactions e componente Header.

--- react/03-money-app/src/App.tsx
+++ react/03-money-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Transaction } from './pages/Transactions'
 
 export function App() {
 
@@ -8,6 +9,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
+      <Transaction />
     </ThemeProvider>
   )
 }

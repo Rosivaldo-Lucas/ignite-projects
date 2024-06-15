6c8582f5b4312070cdf06366eecacd22753cc9a4
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-06-15 01:01:10 -0300

    (03-money-app): feat: adiciona tabela de transactions

--- react/03-money-app/src/pages/Transactions/index.tsx
+++ react/03-money-app/src/pages/Transactions/index.tsx
@@ -1,12 +1,55 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
 export function Transaction() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant='income'>
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>15/06/2024</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant='outcome'>
+                  - R$ 59,88
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>15/06/2024</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>15/06/2024</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>15/06/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }

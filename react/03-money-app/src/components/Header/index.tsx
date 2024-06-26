2b60792818b6c404d8430b5bfabdf285d399533f
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-06-25 21:16:37 -0300

    feat: adiciona criacao e estilizacao do modal de nova transacao

--- react/03-money-app/src/components/Header/index.tsx
+++ react/03-money-app/src/components/Header/index.tsx
@@ -1,3 +1,7 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+import NewTransactionModal from '../NewTransactionModal';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
@@ -6,7 +10,13 @@ export function Header() {
       <HeaderContent>
         <h1>LOGO</h1>
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
